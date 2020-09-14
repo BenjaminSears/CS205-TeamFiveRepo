bc7f1ec6fb322c5730d85d9a05b9b7398c11e4d6
ykhan99 <yousef.khan@uvm.edu>
2020-09-14 14:59:59 -0400

    started code

---
+++ Methods.py
@@ -0,0 +1,20 @@
+
+
+
+def main():
+
+    #read csv file
+
+    #call menuOptions
+
+    #call helpMenu
+
+
+main()
+
+def menuOptions():
+    //display options/give user directions
+
+
+def helpMenu():
+  //Display examples of accepatable quiries
