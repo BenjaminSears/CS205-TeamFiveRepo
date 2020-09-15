2c31c6a1ce171ebb2c95cefc268cdaa910f59647
benjamin-sylvester <benjamin.sylvester@uvm.edu>
2020-09-15 17:27:22 -0400

    population of tables complete

--- db_connection_population.py
+++ db_connection_population.py
@@ -1,75 +1,73 @@
 import sqlite3
 import pandas as pd
 from sqlite3 import Error
-
-
-def get_connection(db_file):
+import os.path
+from os import path
+def connect_to_database():
     conn = None
     try:
-        conn = sqlite3.connect(db_file)
+        conn = sqlite3.connect("/Users/bensylvester/Desktop/bensylvester/Desktop/CS205-TeamFiveRepo/CS205_database.db")
         return conn
+
     except Error as e:
-        print("Error: ", e)
+        print(e)
 
-    return conn
-
-
-def create_table(conn, create_tbl_sql):
-    try:
-        c = conn.cursor()
-        c.execute(create_tbl_sql)
-    except Error as e:
-        print("Error: ", e)
-
-def main():
-
-    database_file = r"/Users/jordanmarchese/PycharmProjects/CS205-TeamFiveRepo/CS205_database.db"
+        if conn is not None:
+            conn.close()
 
 
-    airline_file = r"/Users/jordanmarchese/PycharmProjects/CS205-TeamFiveRepo/airine.csv"
-
-    route_file = r"/Users/jordanmarchese/PycharmProjects/CS205-TeamFiveRepo/route.csv"
-
-    airline_df = pd.read_csv(airline_file)
-
-    route_df = pd.read_csv(route_file)
-
-    print(airline_df.head())
-
-    print(route_df.head())
-
-    create_airline_table_sql = """
-    CREATE TABLE IF NOT EXISTS airline (
-	airline_id integer PRIMARY KEY,
-	name text NOT NULL,
-	icao text
-    );
-    """
-
-    create_route_table_sql = """
-        CREATE TABLE IF NOT EXISTS route (
-    	route_id integer PRIMARY KEY,
-    	airline text,
-    	airline_id integer FOREIGN KEY,
-    	source_airport text,
-    	source_airport_id integer,
-    	destination_airport text,
-    	destination_airport_id integer
-        );
-        """
-
-    conn = get_connection(database_file)
+def insert_values_to_table(table_name, csv_file, column_list):
+    conn = connect_to_database()
 
     if conn is not None:
+        c = conn.cursor()
+        if table_name == "airline":
+            c.execute('CREATE TABLE IF NOT EXISTS ' + table_name +
+                      '(airline_id        ID,'
+                      'name        VARCHAR,'
+                      'icao        VARCHAR)')
 
-        create_table(conn, create_airline_table_sql)
-        create_table(conn, create_route_table_sql)
+        if table_name == "route":
+            c.execute('CREATE TABLE IF NOT EXISTS ' + table_name +
+                      '(route_id                INTEGER,'
+                      'airline                  VARCHAR,'
+                      'airline_id               INTEGER,'
+                      'source_airport           VARCHAR,'
+                      'source_airport_id        INTEGER,'
+                      'destination_airport      VARCHAR,'
+                      'destination_airport_id   INTEGER)')
+        df = pd.read_csv(csv_file)
+        df.columns = column_list
+
+        df.to_sql(name=table_name, con=conn, if_exists='append', index=False)
+
+        conn.close()
+        print('SQL insert process finished')
 
     else:
-        print("ERROR")
+        print('Connection to database failed')
+
+
+def execute_cursor_all_rows(sql_statement):
+    conn = connect_to_database()
     c = conn.cursor()
-    print(c.execute("SELECT * FROM airline"))
-    print(c.execute("SELECT * FROM route"))
+    c.execute(sql_statement)
+    return c.fetchall()
+
+def main():
+    database_file = "/Users/bensylvester/Desktop/bensylvester/Desktop/CS205-TeamFiveRepo/CS205_database.db"
+    airline_csv = "/Users/bensylvester/Desktop/bensylvester/Desktop/CS205-TeamFiveRepo/Airline.csv"
+    route_csv = "/Users/bensylvester/Desktop/bensylvester/Desktop/CS205-TeamFiveRepo/Route.csv"
+    if path.exists(database_file):
+        print(execute_cursor_all_rows("SELECT * FROM airline"))
+        print(execute_cursor_all_rows("SELECT * FROM route"))
+    else:
+
+        airline_column_list = ['airline_id', 'name', 'icao']
+        route_column_list = ['route_id', 'airline', 'airline_id', 'source_airport', 'source_airport_id', 'destination_airport', 'destination_airport_id']
+        insert_values_to_table("airline", airline_csv, airline_column_list)
+        insert_values_to_table("route", route_csv, route_column_list)
+
 
 
 main()
