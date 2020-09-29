49ef45765aeda94c38a9de58b64369e351e34c5f
benjamin-sylvester <benjamin.sylvester@uvm.edu>
2020-09-29 16:20:40 -0400

    finishing touches

--- final_main.py
+++ final_main.py
@@ -1,13 +1,16 @@
 import sqlite3
 import pandas as pd
 from sqlite3 import Error
-import os.path
 from os import path
 
-# global veriables
+# global variables must be assigned values
 DATABASE_FILE = "/Users/bensylvester/Desktop/bensylvester/Desktop/CS205-TeamFiveRepo/CS205_database.db"
 AIRLINE_CSV_FILE = "/Users/bensylvester/Desktop/bensylvester/Desktop/CS205-TeamFiveRepo/Airline.csv"
 ROUTE_CSV_FILE = "/Users/bensylvester/Desktop/bensylvester/Desktop/CS205-TeamFiveRepo/Route.csv"
+
+AIRLINE_COLUMN_LIST = ['airline_id', 'name', 'icao']
+ROUTE_COLUMN_LIST = ['route_id', 'airline', 'airline_id', 'source_airport', 'source_airport_id',
+         'destination_airport', 'destination_airport_id']
 
 def connect_to_database():
     conn = None
@@ -28,13 +31,14 @@ def insert_values_to_table(table_name, csv_file, column_list):
     if conn is not None:
         c = conn.cursor()
         if table_name == "airline":
+            print("CREATING_TABLE: " + table_name)
             c.execute('CREATE TABLE IF NOT EXISTS ' + table_name +
                       '(airline_id        ID,'
                       'name        VARCHAR,'
                       'icao        VARCHAR)')
 
         if table_name == "route":
-            print("CREATING_TABLE")
+            print("CREATING_TABLE: " + table_name)
             c.execute('CREATE TABLE IF NOT EXISTS ' + table_name +
                       '(route_id                INTEGER,'
                       'airline                  VARCHAR,'
@@ -43,14 +47,19 @@ def insert_values_to_table(table_name, csv_file, column_list):
                       'source_airport_id        INTEGER,'
                       'destination_airport      VARCHAR,'
                       'destination_airport_id   INTEGER)')
-        df = pd.read_csv(csv_file)
-        df.columns = column_list
+        try:
+            df = pd.read_csv(csv_file)
+            df.columns = column_list
 
-        df.to_sql(name=table_name, con=conn, if_exists='append', index=False)
+            df.to_sql(name=table_name, con=conn, if_exists='append', index=False)
 
-        conn.close()
-        print('SQL insert process finished')
-
+            conn.close()
+            print('SQL insert process finished for table: ' + table_name)
+            return True
+        except FileNotFoundError as e:
+            print("ERROR: ", e)
+            print("Unable to create table: " + table_name)
+            return False
     else:
         print('Connection to database failed')
 
@@ -66,7 +75,8 @@ def menuOptions():
     print("Welcome to the SQL interpreter! If the data is loaded you may input a request. "
           "Your database will be loaded if it does not already exist'.\nYou may input 'Help' for our help menu"
           "If your session is complete, please enter 'Q' to exit the interface")
-    loadData()
+    if not loadData():
+        return "ERROR LOADING DATA"
      # Prompt user for input
     userInput = input("Please input your request:\n")
     # Check for hard coded inputs 'Help' and 'Load Data'
@@ -89,7 +99,7 @@ def menuOptions():
                              'destination_airport', 'destination_airport_id']
         for i in range(len(parsedUserRequest)):
             parsedUserRequest[i] = parsedUserRequest[i].strip()
-
+        sql = ""
         is_airline_query = False
         is_route_query = False
         is_join_query = False
@@ -101,6 +111,8 @@ def menuOptions():
             is_join_query = True
         if parsedUserRequest[0].lower() in route_column_list and parsedUserRequest[1].lower() not in route_column_list:
             is_join_query = True
+
+
         if len(parsedUserRequest) == 3:
             if is_airline_query:
 
@@ -142,8 +154,10 @@ def menuOptions():
 
 
 
-            print(sql)
-            values = set((execute_cursor_all_rows(sql)))
+        values = set((execute_cursor_all_rows(sql)))
+        if len(values) == 0:
+            print("You have entered either an invalid request, or none of the data meets the critera you inputted. Please try again")
+        else:
             print("Here is the data you have requested:")
             print("Showing the " + parsedUserRequest[0] + " where " + parsedUserRequest[1] + " is equal to " + parsedUserRequest[2] )
             for x in values:
@@ -164,29 +178,53 @@ def helpMenu():
     # Display examples of acceptable queries
     userInput = ""
     while userInput != 'b':
-        userInput = input("This is our help menu! Press 'b' to return to the main menu at any time. Below is our syntax for inputs.\n"
+        print("If you are looking for sample data points to use with our interpreter, enter 'Show Table'\n")
+        print("This is our help menu! Press 'b' to return to the main menu at any time. Below is our syntax for inputs.\n"
                           "The first input is what you are SEARCHING FOR, the second is what you are SEARCHING BY "
-                          "and the third input is the NAME of what you want to search by. \nBelow is an example query of "
+                          "and the third input is the NAME of what you want to search by. \n\nBelow is an example query of "
                           "searching for the ICAO, by flight name, for 135 Airways. The terminal should display 'GNL'\n"
-                          "[ICAO, Name, 135 Airways]\n")
+                          "[ICAO, Name, 135 Airways]\n"
+                          "\nIf you are looking to retrieve entries across both tables, we have an option for that. An example of this would be"
+                          " inputting \n[source_airport, icao, GNL].\nThis would return the source airports for every entry that has ICAO equal to GNL")
+
+        userInput = input("\n\nHope you found this helpful! Enter 'Show table' to view a table, or enter 'b' to return to our interpreter: ")
+        if userInput.lower() == "show table":
+            which_table = input("Enter the table you wish to see all data for ('R' for Route and 'A' for Airline) : ")
+            if which_table.lower() == 'r':
+                sql = "SELECT * FROM route"
+                print(ROUTE_COLUMN_LIST)
+            if which_table.lower() == 'a':
+                sql = "SELECT * FROM airline"
+                print(AIRLINE_COLUMN_LIST)
+            while which_table.lower() != 'r' and which_table.lower() != 'a':
+                which_table = input("Invalid entry. Enter 'R' to view all entries for Route, enter 'A' to view all entries for Airline")
+            values = (execute_cursor_all_rows(sql))
+            for val in values:
+                print(val)
     menuOptions()
 
 
 def loadData():
     # Load the data from either Route.csv or Airline.csv
-
+    success_load = True
     if path.exists(DATABASE_FILE):
         print("Data has already been loaded")
 
     else:
 
-        airline_column_list = ['airline_id', 'name', 'icao']
-        route_column_list = ['route_id', 'airline', 'airline_id', 'source_airport', 'source_airport_id',
-                             'destination_airport', 'destination_airport_id']
-        insert_values_to_table("airline", AIRLINE_CSV_FILE, airline_column_list)
-        insert_values_to_table("route", ROUTE_CSV_FILE, route_column_list)
 
-        print("Data loaded\n")
+        if not insert_values_to_table("airline", AIRLINE_CSV_FILE, AIRLINE_COLUMN_LIST):
+            print("Sorry, you inputted a CSV file that does not exist. Please re-run program to try again")
+            success_load = False
+        if not insert_values_to_table("route", ROUTE_CSV_FILE, ROUTE_COLUMN_LIST):
+            print("Sorry, you inputted a CSV file that does not exist. Please re-run program to try again")
+            success_load = False
+
+        if success_load:
+            print("Data loaded\n")
+            return success_load
+        else:
+            return success_load
 
 def main():
 
